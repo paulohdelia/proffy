79231191d12322719db5890903fe2e8b884ddb6c
paulohdelia <paulohdelia@gmail.com>
2020-08-04 18:03:27 -0300

    feat(api): add function to create a connection

---
+++ server/src/controllers/ConnectionsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import db from '../database/connection';
+
+export default {
+  async index(request: Request, response: Response) {
+
+  },
+
+  async create(request: Request, response: Response) {
+    const { user_id } = request.body;
+
+    await db('connections').insert({
+      user_id,
+    });
+
+    return response.status(201).json({
+      'message': 'Success'
+    })
+  }
+}
