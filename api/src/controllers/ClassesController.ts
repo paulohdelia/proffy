0e2d8ad1968f00cbc3589e9d71e93c7a49709cea
paulohdelia <paulohdelia@gmail.com>
2020-08-06 14:45:05 -0300

    fix(api): change return of get classes

--- api/src/controllers/ClassesController.ts
+++ api/src/controllers/ClassesController.ts
@@ -39,9 +39,9 @@ export default {
       .join('users', 'classes.user_id', '=', 'users.id')
       .select(['classes.*', 'users.*']);
 
-    return response.status(200).json({
+    return response.status(200).json(
       classes
-    })
+    );
   },
 
   async create(request: Request, response: Response) {
