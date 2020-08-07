748caa571099c476cf148210e919109556ac4c7c
paulohdelia <paulohdelia@gmail.com>
2020-08-07 01:05:09 -0300

    fix(mobile): tab bar looks strange on ios

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Platform } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
@@ -12,21 +13,24 @@ function StudyTabs() {
   return (
     <Navigator
       tabBarOptions={{
-
         style: {
           elevation: 0,
           shadowOpacity: 0,
-          height: 64,
+          height: Platform.OS === 'ios' ? 84 : 64,
         },
         tabStyle: {
           flexDirection: 'row',
           alignItems: 'center',
           justifyContent: 'center',
+          paddingBottom: Platform.OS === 'ios' ? 20 : 0,
+        },
+        safeAreaInsets: {
+          bottom: 0,
         },
         iconStyle: {
           flex: 0,
-          height: 20,
           width: 20,
+          height: Platform.OS === 'ios' ? 24 : 20,
         },
         labelStyle: {
           fontFamily: 'Archivo_700Bold',
